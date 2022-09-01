e42edeb6cd94b4095231e0cad2ff531d5948e153
Anurag Modi <anuragmodi@Anurags-MacBook-Air.local>
2022-09-01 15:41:58 +0530

    successfull change 1.0

--- routes/files.js
+++ routes/files.js
@@ -33,7 +33,7 @@ router.post('/',(req,res)=>{
             size : req.file.size
         })
         const response = await file.save();
-        return res.json({file:`${process.env.APP_BASE_URL}/files/${response.uuid}`})
+        return res.json({file:`${process.env.APP_BASE_URL}files/${response.uuid}`})
     })
 
 })
@@ -61,7 +61,7 @@ router.post('/send',async(req,res)=>{
         text: `${emailFrom} shared a file with you.`,
         html: require('../services/emailTemplate')({
             emailFrom:emailFrom,
-            downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+            downloadLink: `${process.env.APP_BASE_URL}files/${file.uuid}`,
             size: parseInt(file.size/1000)+'KB',
             expires:'24 hours'
         })
